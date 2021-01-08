1101e3efd0099e2b0845b7ca03f2c7575d775a02
xiongzhen1234 <xiongzhen1995>
2021-01-08 16:06:05 +0800

    springboot集成kafka

---
+++ src/main/java/com/example/springbootdemo/kafka/KafkaConfig.java
@@ -0,0 +1,120 @@
+package com.example.springbootdemo.kafka;
+
+import lombok.Data;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Description: </br>
+ * <p>版权所有：</p>
+ * 未经本公司许可，不得以任何方式复制或使用本程序任何部分
+ * <p>
+ * 杭州孚立计算机软件有限公司
+ *
+ * @author LargerBear</ br>
+ * date: 2020/12/17 14:28</br>
+ * @since JDK 1.8
+ */
+@Configuration
+@EnableKafka
+@Data
+public class KafkaConfig {
+    @Value("${kafka.broker-list}")
+    private String brokers;
+    @Value("${kafka.producer.retries}")
+    private Integer producer_retries;
+    @Value("${kafka.producer.batch-size}")
+    private Integer producer_batch_size;
+    @Value("${kafka.producer.linger-ms}")
+    private Integer producer_linger_ms;
+    @Value("${kafka.producer.buffer-memory}")
+    private Integer producer_buffer_memory;
+    @Value("${kafka.producer.key-serializer}")
+    private String producer_key_serializer;
+    @Value("${kafka.producer.value-serializer}")
+    private String producer_value_serializer;
+    @Value("${kafka.producer.topic}")
+    private String producer_topic;
+
+    @Value("${kafka.consumer.topic}")
+    private String consumer_topic;
+    @Value("${kafka.consumer.group-id}")
+    private String consumer_group_id;
+    @Value("${kafka.consumer.enable-auto-commit}")
+    private boolean consumer_enable_auto_commit;
+    @Value("${kafka.consumer.auto-commit-ms}")
+    private String consumer_auto_commit_ms;
+    @Value("${kafka.consumer.session-timeout-ms}")
+    private String consumer_session_timeout_ms;
+    @Value("${kafka.consumer.key-deserializer}")
+    private String consumer_key_deserializer;
+    @Value("${kafka.consumer.value-deserializer}")
+    private String consumer_value_deserializer;
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    /**
+     * 消费者参数配置
+     * @return
+     */
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>(8);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, consumer_group_id);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, consumer_enable_auto_commit);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, consumer_auto_commit_ms);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, consumer_session_timeout_ms);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, consumer_key_deserializer);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, consumer_value_deserializer);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    /**
+     * 生产者参数配置
+     * @return
+     */
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>(8);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        props.put(ProducerConfig.RETRIES_CONFIG, producer_retries);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, producer_batch_size);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, producer_linger_ms);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, producer_buffer_memory);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, producer_key_serializer);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, producer_value_serializer);
+        return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        System.out.println("init");
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
+
---
+++ src/main/java/com/example/springbootdemo/kafka/KafkaService.java
@@ -0,0 +1,129 @@
+package com.example.springbootdemo.kafka;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import javax.annotation.Resource;
+import java.util.*;
+
+/**
+ * Description: </br>
+ * <p>版权所有：</p>
+ * 未经本公司许可，不得以任何方式复制或使用本程序任何部分
+ * <p>
+ * 杭州孚立计算机软件有限公司
+ *
+ * @author LargerBear</ br>
+ * date: 2020/12/17 14:32</br>
+ * @since JDK 1.8
+ */
+@Service
+@Slf4j
+public class KafkaService {
+
+    @Resource
+    private KafkaConfig config;
+
+
+
+    /**
+     *异步发送消息(默认topic为配置文件中的topic)
+     * @param msg
+     */
+    public void sendAsyncMsg(String msg) throws RuntimeException{
+        log.info("准备发送消息为：{}", msg);
+        //发送消息
+        this.sendAsyncMsg(config.getProducer_topic(), msg);
+    }
+
+    /**
+     *异步发送消息
+     * @param topic
+     * @param msg
+     */
+    public void sendAsyncMsg(String topic, String msg) throws RuntimeException{
+        log.info("准备发送消息为：{}", msg);
+        log.info("发送topic为：{}", topic);
+        //发送消息
+        ListenableFuture<SendResult<String, Object>> future = config.kafkaTemplate().send(topic,"data", msg);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                //发送失败的处理
+                log.info(topic + " - 生产者 发送消息失败：" + throwable.getMessage());
+                throw new RuntimeException("kafka发送消息失败");
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> stringObjectSendResult) {
+                //成功的处理
+                log.info(topic + " - 生产者 发送消息成功：" + stringObjectSendResult.toString());
+            }
+        });
+    }
+
+    /**
+     * 同步发送消息(默认topic为配置文件中的topic)
+     * @param msg
+     * @return
+     * @throws Exception
+     */
+    public SendResult<String, Object> sendSyncMsg(String msg) throws Exception{
+        return config.kafkaTemplate().send(config.getProducer_topic(), "data", msg).get();
+    }
+
+    /**
+     *同步发送消息
+     * @param topic
+     * @param msg
+     * @return
+     * @throws Exception
+     */
+    public SendResult<String, Object> sendSyncMsg(String topic, String msg) throws Exception{
+        return config.kafkaTemplate().send(topic, "data", msg).get();
+    }
+
+//    /**
+//     *指定topic和拉取时间消费消息
+//     * @param topic
+//     * @param time
+//     * @return
+//     */
+//    public ConsumerRecords<String, Object> pollMsg(String topic, long time){
+//        Consumer<String, Object> consumer = config.consumerFactory().createConsumer();
+//        consumer.subscribe(Arrays.asList(topic,","));
+//        while (true) {
+//            return consumer.poll(Duration.ofMillis(time));
+//        }
+//    }
+
+
+
+
+    /**
+     *
+     * kafka消息监听接收待办任务
+     * @param record
+     * @param topic
+     */
+    @KafkaListener(topics = "${kafka.consumer.topic}", groupId = "${kafka.consumer.group-id}")
+    public void consumerTask(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            JSONObject msg = JSONObject.parseObject(message.get().toString());
+            log.info("topic_test1 消费了： Topic:" + topic + ",Message:" + msg);
+            //业务处理 消息接收持久化到数据库
+            //设置自动提交
+//            ack.acknowledge();
+        }
+    }
+
+}
